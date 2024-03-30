b8d367cfbffd322d288608436f0dfd0ed7d9762b
Kaic Salomão <imkaicsm@gmail.com>
2024-03-30 13:52:05 -0300

    Corrigindo game.xml e removendo comentários deprecádos

--- rpg_xml/scene_processor.py
+++ rpg_xml/scene_processor.py
@@ -1,6 +1,5 @@
 from .util import type_message
 
-# Atributos existentes para o elemento descricao
 msg_attr = [
     "delay-before",
     "wrap-line",
@@ -18,7 +17,6 @@ class SceneProcessor:
         self._proc_option()
 
     def _proc_description(self):
-        # Percorre todas as descricoes que estao dentro de uma cena
         for msg in self.scene.findall("msg"):
             # Coleta todos os atributos encontrados na tag
             attrs_found = self._get_attr(msg, msg_attr)
@@ -41,7 +39,6 @@ class SceneProcessor:
             )
 
     def _proc_option(self):
-        # TODO: Fazer a logica de processamento para as opcoes
         pass
 
     def _get_attr(self, tag, attr_rules):
@@ -50,6 +47,5 @@ class SceneProcessor:
         for attr in attr_rules:
             attr_value = tag.get(attr)
             if attr_value is not None:
-                # "atraso-antes":"..."
                 attrs_found[attr] = attr_value
         return attrs_found
