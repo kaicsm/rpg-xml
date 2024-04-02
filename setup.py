f5a0d9721d6f0ed629432fcb5c2f4f68de801734
Kaic Salomão <imkaicsm@gmail.com>
2024-04-01 21:51:38 -0300

    Atualizar versão 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rpg_xml",
-    version="0.1.0",
+    version="0.1.1",
     description="Crie rpg de texto usando xml!",
     author="Kaic Salomão",
     packages=find_packages(),
