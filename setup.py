8e6eefe052cdb5a124519595c30c233f786f4016
Kaic Salomão <imkaicsm@gmail.com>
2024-04-03 12:34:08 -0300

    Nova versão corrigida

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rpg_xml",
-    version="0.1.1",
+    version="0.1.2",
     description="Crie rpg de texto usando xml!",
     author="Kaic Salomão",
     packages=find_packages(),
